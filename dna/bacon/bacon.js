5b8372e1dc68b4026d33eb052acf739b2c3ead62
JhonatanHern <jhonatanhernandez998@gmail.com>
2018-11-14 13:57:33 -0400

    debugging

--- dna/bacon/bacon.js
+++ dna/bacon/bacon.js
@@ -189,11 +189,8 @@ function validTransaction(transaction,address) {
         transaction.Entry.from !== T1.Entry.from || // 
         transaction.Entry.to !== T1.Entry.to || // 
         transaction.Entry.from !== address || // current node validation
-        getCreator(T1.Hash) !== transaction.to // response validation
+        getCreator(T1.Hash) !== transaction.Entry.to // response validation
         ) {
-        console.log('invalid coz\':')
-        console.log(transaction.Entry.from !== address)
-        console.log(getCreator(T1.Hash) !== transaction.to)
         return 0//invalid transaction
       }
       return - transaction.Entry.amount
